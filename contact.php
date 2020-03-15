833aa3a5cc0c928001a9bc30d429f8ed1f01bc4d
willem640 <willemschipper2@gmail.com>
2020-03-15 23:53:11 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ require_once 'setup.php';
 <!DOCTYPE HTML>
 <html>
 <?php echo $header; ?>
-<center><p> Hoihoi welkom op de website van Robin's AH bezorgservice.<br> Als je vragen hebt, als je je wachtwoord vergeten bent of als je je account wil verwijderen kan je een mailtje sturen naar <a href="mailto:willemschipper2@gmail.com">willemschipper2@gmail.com</a> of naar <a href="mailto:robinvandijke2002@gmail.com">robinvandijke2002@gmail.com</a><br>
+<center><p> Hoihoi welkom op de website van Robin's AH bezorgservice.<br> Als je vragen hebt, als je je wachtwoord vergeten bent of als je je account wil verwijderen kan je een mailtje of een berichtje sturen. <br>
 </body>
 
 </html>
