03b9b44a71955aca24133c9625976c51cbb74235
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-04-20 10:57:33 +0200

    Mobile header toegevoegd.

--- zoeken.php
+++ zoeken.php
@@ -3,7 +3,7 @@ require_once 'setup.php';?>
 
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153875032-1"></script>
 <script>
@@ -30,30 +30,83 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GJ825S"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-<div class="banner-mobile">
-  <div class="dropdown">
-    <button class="dropbtn"> Robins AH Bestelservice
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-	<ul>
-      <li><a id="mobile_banner_a" href="index.php">Home</a>
-      <li><a id="mobile_banner_a" href="zoek.php">Zoek</a>
-      <li><a id="mobile_banner_a" href="bonus.php">Bonus</a>
-      <li><a id="mobile_banner_a" href="bestelling.php">Bestel</a>
-      <li><a id="mobile_banner_a" href="contact.php">Contact</a>
-      <?php if($logged_in){
-        echo '<li><a id="mobile_banner_a" href="profile.php">Profiel</a>';
-        } else {
-        echo '<li><a id="mobile_banner_a" href="login.php">Inloggen</a>';
-        } ?>
-	</ul>
-    </div>
+<div class="anim">
   </div>
+<div class="banner-mobile">
+<div class="links">
+	<div class="link">
+		<a href="index.php">
+			<?php include("assets/home-24px.svg");?>
+			<p>Home</p>
+		</a>
+	</div>
+	<div class="link">
+		<a href="zoeken.php">
+			<?php include("assets/search-24px.svg");?>
+			<p>Zoek</p>
+		</a>
+	</div>
+	<div class="link">
+		<a href="bonus.php">
+			<?php include("assets/euro_symbol-24px.svg");?>
+			<p>Bonus</p>
+		</a>
+	</div>
+	<div class="link">
+		<a href="bestelling.php">
+			<?php include("assets/shopping_cart-24px.svg");?>
+			<p>Winkelmandje</p>
+		</a>
+	</div>
+	<div class="link">
+		<a href="contact.php">
+			<?php include("assets/contact_support-24px.svg");?>
+			<p>Contact</p>
+		</a>
+	</div>
+	<?php 
+		$logged_in=(isset($_SESSION['loggedin']) ? $_SESSION['loggedin'] : false);
+		if($logged_in){
+        echo '<div class="link">
+			  <a href="profile.php">';
+		include("assets/person_outline-24px.svg");
+		echo '<p>Profiel</p>
+		      </a>
+	          </div>';
+        } else {
+        echo '<div class="link">
+			  <a href="login.php">';
+		include("assets/lock_open-24px.svg");
+		echo '<p>Login</p>
+		      </a>
+			  </div>';
+        }
+	?>
+	
 </div>
+</div>
+  <div class="circle">
+  <img src="assets/android-chrome-512x512.png">
+  </div>
+
+</div>
+  
 <script>
-	$('.dropbtn').on('touchstart', function (event) {
-    $(".dropdown-content").slideToggle(200, "swing");
+	var i = 0;
+	$('.links').fadeOut(0);
+	$('.circle').click(function(){
+		i+=1;
+		if(i%2!=0){
+			$(".wrapper").fadeOut();
+			$('.banner-mobile').animate({height: "100vh"});
+			$('.circle').animate({top: '-=15vh'});
+			$('.links').fadeIn();
+		} else {
+			$(".wrapper").fadeIn();
+			$('.banner-mobile').animate({height: "50vh"});
+			$('.circle').animate({top: '+=15vh'});
+			$('.links').fadeOut();
+		}
 	});
 </script>
         <form method="get">
