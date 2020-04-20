a50f2276bf880894b1d0edd5e71013978dc333d8
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-04-20 10:59:16 +0200

    Viewport shizzle aangepast

--- zoeken.php
+++ zoeken.php
@@ -13,6 +13,7 @@ require_once 'setup.php';?>
 
   gtag('config', 'UA-153875032-1');
 </script>
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
