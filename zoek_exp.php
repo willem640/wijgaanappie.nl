6364914ea68cf9129893a5af9f7f7bef626d7af5
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-04-23 13:04:21 +0200

    Form toegevoegd

--- zoek_exp.php
+++ zoek_exp.php
@@ -1,7 +1,14 @@
-<?php
 
+<?php
+$search=(isset($_POST['zoek']) ?? '');
+echo $search;
 session_start();
 require_once 'setup.php';
-$query=DB::query('SELECT * FROM products LIMIT 100');
+$query=DB::query("SELECT * FROM products WHERE MATCH(title) AGAINST(%s) ORDER BY MATCH(title) AGAINST(%s) DESC", $search, $search);
 var_dump($query);
 ?>
+
+<form>
+    <input type="text" name="zoek"/>
+    <input type="submit"/>
+</form>
