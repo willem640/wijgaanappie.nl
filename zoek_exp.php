8c5aab727319193aae60bf5388bb80cd22b3e03c
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-04-23 13:22:13 +0200

    search output gefixt

--- zoek_exp.php
+++ zoek_exp.php
@@ -1,15 +1,22 @@
 
 <?php
 $search=$_GET['zoek'] ?? '';
-var_dump($_GET);
 echo $search;
 session_start();
 require_once 'setup.php';
 $query=DB::query("SELECT * FROM products WHERE MATCH(title) AGAINST(%s) ORDER BY MATCH(title) AGAINST(%s) DESC", $search, $search);
-var_dump($query);
 ?>
 
 <form method="get" action="zoek_exp.php">
     <input type="text" name="zoek"/>
     <input type="submit"/>
 </form>
+
+<?php
+
+echo '<ul>';
+foreach($query as $result){
+    echo '<li>' . $result['title'] . ' ' . $result['priceNow'] . ' ' . $result['unitSize'] . '</li>';
+}
+echo '</ul>';
+?>
