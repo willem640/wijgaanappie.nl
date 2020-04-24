b9cbd47da1d6517e9d19f68c89e8a41e10aa4ef7
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-04-24 15:20:47 +0200

    Test curl

--- zoek_exp.php
+++ zoek_exp.php
@@ -17,20 +17,30 @@ $query=DB::query("SELECT * FROM products WHERE MATCH(title) AGAINST(%s) ORDER BY
 echo 'Resulaten Aantal Gevonden: ' . count($query);
 echo '<ul>';
 $mh = curl_multi_init();
+$curlHandles=array();
 foreach($query as $result){
     $url="https://www.ah.nl/service/rest" . substr($result['link'], 17, strlen($result['link'])-17);
     $ch=curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $curlHandles[$url]=$ch;
+    
     curl_multi_add_handle($mh, $ch);
+    
 }
 do {
     $status= curl_multi_exec($mh, $active);
-    var_dump($status);
     if($active){
     curl_multi_select($mh);
     }
 } while ($active && $status == CURLM_OK);
+
+foreach($curlHandles as $url=>$ch){
+    $content=json_decode(curl_multi_getcontent($ch));
+    echo '<pre>';
+    print_r($content);
+    echo '</pre>';
+}
 curl_multi_close($mh);
 echo '</ul>';
 ?>
