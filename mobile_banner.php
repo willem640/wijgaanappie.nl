9573a46567d16cd657a8746c0842e0f036d7bc5c
willem <willem@localhost>
2020-05-15 23:15:29 +0200

    .circle in _mobile-banner.scss, en vh en vw in js zodat hij op mijn telefoon niet raar doet

--- mobile_banner.php
+++ mobile_banner.php
@@ -3,25 +3,29 @@ session_start();
 ?>
 
 <script type="text/javascript">
+    var vh = $(window).height() / 100;
+    var vw = $(window).width() / 100;
     $(document).ready(() => {
         var buttons = $('.mdc-button');
         for (var i = 0; i < buttons.length; ++i) {
             mdc.ripple.MDCRipple.attachTo(buttons[i]);
         }
-        
+
         $('#circle').click(() => {
- 
+
             if (!$('#circle').hasClass('open')) {
                 $('body > *').not('.top-mobile-banner').not('.circle').not('script').velocity("fadeOut");
                 $('.top-mobile-banner').velocity({height: "100vh"});
-                $('.circle').velocity({top: '-=15vh'});
+                $('.circle').velocity({top: 10*vh - 12.5*vw}); // calc werkt hier niet
                 $('.banner-links').velocity("fadeIn");
                 $('#circle').addClass('open');
             } else {
                 $('body > *').not('.top-mobile-banner').not('.circle').not('script').velocity("fadeIn");
-                setTimeout(() => {$('body > *').not('.top-mobile-banner').not('.circle').not('script').css("display", "");}, 450);
+                setTimeout(() => {
+                    $('body > *').not('.top-mobile-banner').not('.circle').not('script').css("display", "");
+                }, 450);
                 $('.top-mobile-banner').velocity({height: "50vh"});
-                $('.circle').velocity({top: '+=15vh'});
+                $('.circle').velocity({top: 25*vh - 12.5*vw});
                 $('.banner-links').velocity("fadeOut");
                 $('#circle').removeClass('open');
             }
@@ -78,7 +82,7 @@ session_start();
                 <span class="mdc-button__label">
                     <i class="material-icons-round" aria-hidden="true"><?= ($_SESSION['loggedin'] ? 'account_circle' : 'lock_open') ?></i>
                     <br>
-                    <?= ($_SESSION['loggedin'] ? 'Profiel' : 'Inloggen') ?>
+<?= ($_SESSION['loggedin'] ? 'Profiel' : 'Inloggen') ?>
                 </span>
             </button>
         </div>
