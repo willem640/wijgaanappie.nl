f3007fe42d9c8c30822020db59e9a9a0ee6bd06d
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-08-07 20:22:51 +0200

    Create status.php

---
+++ status.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+require_once 'setup.php';
+require_once 'header.php';
+  $query = DB::query('SELECT * FROM komt_chobin_naar_de_appie')[0];
+  $komt_chobin = $query['komt hij'];
+  
+  $array = [];
+  $array['status'] = $komt_chobin;
+  $json = json_encode(array);
+  echo($json);
+?>
