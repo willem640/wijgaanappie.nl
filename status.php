78bb795a8f4a455d298cc5c1a8d108518072df0a
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-08-07 20:32:26 +0200

    Update status.php

--- status.php
+++ status.php
@@ -4,7 +4,7 @@ require_once 'setup.php';
 require_once 'header.php';
 $query = DB::query('SELECT * FROM komt_chobin_naar_de_appie')[0];
 $komt_chobin = $query['komt hij'];
-if($komt_chobin == 2){
+if($komt_chobin == '2'){
   $array['special_status'] == $query['special_status'];
 }
 $array = [];
