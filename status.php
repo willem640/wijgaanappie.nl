0dc0f4ffbe17621eb2405c53342d50b291dfc3e1
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-08-07 20:25:32 +0200

    Update status.php

--- status.php
+++ status.php
@@ -2,11 +2,11 @@
 session_start();
 require_once 'setup.php';
 require_once 'header.php';
-  $query = DB::query('SELECT * FROM komt_chobin_naar_de_appie')[0];
-  $komt_chobin = $query['komt hij'];
-  
-  $array = [];
-  $array['status'] = $komt_chobin;
-  $json = json_encode(array);
-  echo($json);
+$query = DB::query('SELECT * FROM komt_chobin_naar_de_appie')[0];
+$komt_chobin = $query['komt hij'];
+
+$array = [];
+$array['status'] = $komt_chobin;
+$json = json_encode($array);
+echo($json);
 ?>
