c82768172e4e071bd6a5b9de2339eaee11963366
robin <robin@DESKTOP-3LMI1SN>
2020-08-26 21:38:32 +0200

    Ben op ww vergeten aan het verdergaan

--- wachtwoord_vergeten.php
+++ wachtwoord_vergeten.php
@@ -8,7 +8,7 @@ require 'PHPMailer/SMTP.php';
 var_dump($_GET);
 if(isset($_GET['email']) && isset($_GET['username'])){
     $user = DB::queryFirstRow('SELECT `username`,`email` FROM `users` WHERE `username` = %s0 AND `email` = %s1', $_GET['email'], $_GET['username']);
-    $user = ['username' => 'test_die_niet_bestaat', 'email' => 'test-4577zku20@srv1.mail-tester.com'];
+    //$user = ['username' => 'test_die_niet_bestaat', 'email' => 'test-4577zku20@srv1.mail-tester.com'];
     if(isset($user)){
         $token = bin2hex(openssl_random_pseudo_bytes(127));
         $valid_till = new DateTime();
