c46afc0241173968fb60d2dce86898fb50bee819
robin <robin@DESKTOP-3LMI1SN>
2020-08-27 16:45:56 +0200

    zou nu moeten verifyen

--- reset_wachtwoord.php
+++ reset_wachtwoord.php
@@ -6,10 +6,10 @@ if(isset($_GET['username']) && isset($_GET['token'])){
     $token = DB::QueryFirstRow('SELECT token FROM forgot_password WHERE email=%s', $_GET['username']);
     $timestamp = DB::QueryFirstRow('SELECT valid_till FROM forgot_password WHERE email=%s', $_GET['username'])['valid_till'];
     $now = new DateTime();
-    print_r($timestamp);
-    /*if($now < DateTime($timestamp) && $token==$_GET['token']){
+    
+    if($now < DateTime($timestamp) && $token==$_GET['token']){
         echo 'succesfully verified';
-    }*/
+    }
     var_dump($_GET);
 }
 ?>
