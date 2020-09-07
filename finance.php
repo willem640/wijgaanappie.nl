d15d1641de21dc1bb75a1617c8d5b8509c850dda
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-09-07 14:49:29 +0200

    Update finance.php

--- finance.php
+++ finance.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 require_once 'setup.php';
-$start_date=(date("D")=="Mon" ? strtotime("today") : strtotime("last monday"));
 $perm_level = DB::query("SELECT perm_level FROM users WHERE username = %s", $_SESSION['username'])[0]['perm_level'];
 if($perm_level != 2){header("Location: login.php");}
 $contents=(isset($_GET['date'] ? json_decode(DB::query("SELECT all_orders FROM finance WHERE date=%s", $_GET['date']), true) : []);
+var_dump($contents);
 ?>
 
 <!DOCTYPE HTML>
@@ -21,7 +21,7 @@ $contents=(isset($_GET['date'] ? json_decode(DB::query("SELECT all_orders FROM f
 </form>
 <div id="card">
     <?php
-    var_dump($contents);
+    //var_dump($contents);
     ?>
 </div>
 </div>
