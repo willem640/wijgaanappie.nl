0a7a33f72513db8f56ffe130ef4c8c00f2cd8e52
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-09-07 15:15:28 +0200

    Beginning of list

--- finance.php
+++ finance.php
@@ -3,9 +3,7 @@ session_start();
 require_once 'setup.php';
 $perm_level = DB::query("SELECT perm_level FROM users WHERE username = %s", $_SESSION['username'])[0]['perm_level'];
 if($perm_level != 2){header("Location: login.php");}
-$contents=(isset($_GET['date']) ? json_decode(DB::queryFirstRow("SELECT all_orders FROM finance WHERE date=%s", $_GET['date'])['all_orders'], true) : []);
-print_r($contents);
-print_r(gettype($_GET['date']));
+$all_orders=(isset($_GET['date']) ? json_decode(DB::queryFirstRow("SELECT all_orders FROM finance WHERE date=%s", $_GET['date'])['all_orders'], true) : []);
 ?>
 
 <!DOCTYPE HTML>
@@ -21,8 +19,24 @@ print_r(gettype($_GET['date']));
     <input type="submit">
 </form>
 <div id="card">
+    <h1><?php echo $_GET['date']?>
     <?php
-    //var_dump($contents);
+    echo '<div class="mdc-card material-card lijst" id="tikkie">';
+            echo '<ul class="mdc-list">';
+            foreach($all_orders as $order){
+                $user = (empty($order['realname']) ? $order['username'] : $order['realname']);
+                $contents=json_decode($order['contents'], true);
+                $tot = 0;
+                foreach($contents as $prod){
+                    $am = $prod['bestelling_amount'];
+                    $tot+=1.1*($prod['priceLabel']['now']*$am);
+                }
+                echo '<li class="mdc-list-item" tabindex="0">'; //List item
+                echo '<span class="mdc-list-item__text">' . $user . '</span>'; //Primary text   
+                echo '<span class="mdc-list-item__meta">' . round($tot, 2) . '</span>'; //Meta tag
+            }
+            echo '</ul>';
+            echo '</div>';
     ?>
 </div>
 </div>
