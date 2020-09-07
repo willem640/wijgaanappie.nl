c1f6c2573fbf413df4569df7df234ed6f0d48c3e
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-09-07 14:46:23 +0200

    Beginnetje finance

--- finance.php
+++ finance.php
@@ -2,6 +2,9 @@
 session_start();
 require_once 'setup.php';
 $start_date=(date("D")=="Mon" ? strtotime("today") : strtotime("last monday"));
+$perm_level = DB::query("SELECT perm_level FROM users WHERE username = %s", $_SESSION['username'])[0]['perm_level'];
+if($perm_level != 2){header("Location: login.php");}
+$contents=(isset($_GET['date'] ? json_decode(DB::query("SELECT all_orders FROM finance WHERE date=%s", $_GET['date']), true) : []);
 ?>
 
 <!DOCTYPE HTML>
@@ -16,10 +19,11 @@ $start_date=(date("D")=="Mon" ? strtotime("today") : strtotime("last monday"));
     <input type="date" value="<?php echo $_POST['date'] ?? date('Y-m-d')?>" name="date">
     <input type="submit">
 </form>
-<?php
-$perm_level = DB::query("SELECT perm_level FROM users WHERE username = %s", $_SESSION['username'])[0]['perm_level'];
-if($perm_level != 2){header("Location: login.php");}
-?>
+<div id="card">
+    <?php
+    var_dump($contents);
+    ?>
+</div>
 </div>
 </body>
 
