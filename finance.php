8eed85816febe71250c16e6426554e9ab59db4ef
robin <robin@DESKTOP-3LMI1SN>
2020-09-09 23:28:48 +0200

    Removed unnecessary div and reformatted code

--- finance.php
+++ finance.php
@@ -2,43 +2,43 @@
 session_start();
 require_once 'setup.php';
 $perm_level = DB::query("SELECT perm_level FROM users WHERE username = %s", $_SESSION['username'])[0]['perm_level'];
-if($perm_level != 2){header("Location: login.php");}
-$all_orders=(isset($_GET['date']) ? json_decode(DB::queryFirstRow("SELECT all_orders FROM finance WHERE date=%s", $_GET['date'])['all_orders'], true) : []);
+if ($perm_level != 2) {
+    header("Location: login.php");
+}
+$all_orders = (isset($_GET['date']) ? json_decode(DB::queryFirstRow("SELECT all_orders FROM finance WHERE date=%s", $_GET['date'])['all_orders'], true) : []);
 ?>
 
 <!DOCTYPE HTML>
 <html>
     <head>
-        <?php include 'header_material.php'?>
-</head>
-<body>
-    <?php include 'mobile_banner.php'?>
-<div class="wrapper">
-<form method="get">
-    <input type="date" value="<?php echo $_POST['date'] ?? date('Y-m-d')?>" name="date">
-    <input type="submit">
-</form>
-<div id="card">
-    <h1><?php echo $_GET['date']?></h1>
-    
-    <?php
-    echo '<div class="mdc-card material-card lijst" id="tikkie">';
-            echo '<ul class="mdc-list">';
-            foreach($all_orders as $username=>$order){
-                $tot = 0;
-                foreach($order as $prod){
-                    $am = $prod['bestelling_amount'];
-                    $tot+=1.1*($prod['priceLabel']['now']*$am);
+        <?php include 'header_material.php' ?>
+    </head>
+    <body>
+<?php include 'mobile_banner.php' ?>
+        <div class="wrapper">
+            <form method="get">
+                <input type="date" value="<?php echo $_POST['date'] ?? date('Y-m-d') ?>" name="date">
+                <input type="submit">
+            </form>
+            <div id="card">
+                <h1><?php echo $_GET['date'] ?></h1>
+
+                <?php
+                echo '<ul class="mdc-list">';
+                foreach ($all_orders as $username => $order) {
+                    $tot = 0;
+                    foreach ($order as $prod) {
+                        $am = $prod['bestelling_amount'];
+                        $tot += 1.1 * ($prod['priceLabel']['now'] * $am);
+                    }
+                    echo '<li class="mdc-list-item" tabindex="0">'; //List item
+                    echo '<span class="mdc-list-item__text">' . $username . '</span>'; //Primary text   
+                    echo '<span class="mdc-list-item__meta">' . round($tot, 2) . '</span>'; //Meta tag
                 }
-                echo '<li class="mdc-list-item" tabindex="0">'; //List item
-                echo '<span class="mdc-list-item__text">' . $username . '</span>'; //Primary text   
-                echo '<span class="mdc-list-item__meta">' . round($tot, 2) . '</span>'; //Meta tag
-            }
-            echo '</ul>';
-            echo '</div>';
-    ?>
-</div>
-</div>
-</body>
+                echo '</ul>';
+                ?>
+            </div>
+        </div>
+    </body>
 
 </html>
