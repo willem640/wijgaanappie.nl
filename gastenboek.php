9fe67f95e1fa10eaf6a0461b3242795ae85ea49f
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-11-30 14:15:47 +0100

    Update gastenboek.php

--- gastenboek.php
+++ gastenboek.php
@@ -3,7 +3,7 @@ require_once('setup.php');
 $results = DB::query('SELECT * FROM gastenboek ORDER BY id DESC');
 foreach($results as $result){
   foreach($result as $line){
-    echo($line);
+    echo '$line<br>';
   }
   echo '<hr>';
 }
