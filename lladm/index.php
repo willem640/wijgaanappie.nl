7d47494b22eff040aa7e41078d926802702e764b
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-12-07 14:25:38 +0100

    Update index.php

--- lladm/index.php
+++ lladm/index.php
@@ -5,7 +5,20 @@
 <html>
   <h1>Leerlingadministratie Breeweg</h1>
   <hr>
-  <table>
+  <table border="1" cellpadding="5" cellspacing="5" style="
+    width: 100%;>
+      <tr>
+        <th>Leerlingnummer</th>
+        <th>Voornaam</th>
+        <th>Achternaam</th>
+        <th>Dag</th>
+        <th>Maand</th>
+        <th>Jaar</th>
+        <th>Straat</th>
+        <th>Huisnummer</th>
+        <th>Postcode</th>
+        <th>Stad</th>
+      </tr>
     <?php
     $results = DB::query('SELECT * FROM lijst');
     foreach($results as $row){
