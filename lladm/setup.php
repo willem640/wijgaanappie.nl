a070fd0306dfec3a15eecef06f8b31ea7bc94be4
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-12-07 14:09:57 +0100

    Create setup.php

---
+++ lladm/setup.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'meekrodb.2.3.class.php';
+DB::$user = 'u63467p60473_project.appie';
+DB::$password = '1HebtwDyk6vDLwaBqrhvkb0qo97MHZR8mvPFmAVC';
+DB::$dbName = 'u63467p60473_appiebezorging';
+//DB::debugMode();
+date_default_timezone_set("CET");
+
+$customSearch['apiKey'] = 'AIzaSyCsthUKCLZxBYkdOnxyNL77-VuNR2VfVhM';
+$customSearch['searchID'] = '005592780774263392923:n95imdntmnb';
