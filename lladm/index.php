0943de001d99ab3ad74e1e780706610b41a56b98
CodeNamedRobin <robinvandijke2002@gmail.com>
2020-12-07 14:01:05 +0100

    Update index.php

--- lladm/index.php
+++ lladm/index.php
@@ -1,1 +1,13 @@
-placeholder
+<?php
+  require_once('setup.php');
+?>
+<html>
+  <h1>Leerlingadministratie Breeweg</h1>
+  <hr>
+  <table>
+    $results = DB::query('SELECT * FROM lijst');
+    foreach($results as $row){
+      echo $row;
+    }
+  </table>
+</html>
